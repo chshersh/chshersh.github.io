da3b2c24641651d110454033ee26ef0b9e74b957
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-11-06 18:29:45 +0000

    [#46] Update 'master' deployed branch to 'main'
    Resolves #46

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,15 +7,15 @@ cabal run site rebuild
 # Create deploy environment inside of .deploy directory
 mkdir .deploy
 cd .deploy
-git init
+git init --initial-branch=main
 git remote add origin git@github.com:chshersh/chshersh.github.io.git
-git pull -r origin master
+git pull -r origin main
 
 # Add built site files
 rsync -a ../_site/ .
 git add .
 git commit -m "$1"
-git push origin master
+git push origin main
 
 # Cleanup .deploy directory after a successful push
 cd ..
